9cd8f157425eb98581b8d6a901ace6714cfa2a14
Vegard Storheil Eriksen <zyp@jvnv.net>
2024-11-01 14:34:16 +0100

    lib.gcc: Learn module dependencies through module mapper.

--- erect/lib/gcc/__init__.py
+++ erect/lib/gcc/__init__.py
@@ -37,20 +37,26 @@ class Env(core.Env):
         self.include_path = []
 
         if cxx_modules:
-            self.module_mapper = ModuleMapper(self.build_dir / 'cmi')
+            self.module_mapper = ModuleMapper(self, self.build_dir / 'cmi')
             self.ctx.start_async(self.module_mapper.start())
         else:
             self.module_mapper = None
 
+    def _task_from_ident(self, ident):
+        if ident.endswith('.cpp'):
+            return self.ctx.tasks.get(core.TaskID('compile', self.build_dir, ident))
+        else:
+            return self.ctx.tasks.get(core.TaskID('header_module', self.build_dir, ident))
+
     def executable(self, output_file, source_files, **kwargs):
         return Link(self, output_file, [pathlib.Path(f) for f in source_files], **kwargs)
 
-class ScanDeps(core.Task):
+class Compile(core.Task):
     env: Env
 
-    def __new__(cls, env: Env, source_file):
+    def __new__(cls, env, source_file):
         try:
-            self = super().__new__(cls, env.ctx, ('scan_deps', env.build_dir, source_file))
+            self = super().__new__(cls, env.ctx, ('compile', env.build_dir, source_file))
         except core.TaskExists as e:
             if e.task.env == env:
                 return e.task
@@ -59,9 +65,10 @@ class ScanDeps(core.Task):
         self.env = env
         self.source_file = source_file
         self.object_file = self.env.build_dir / 'objects' / source_file.with_suffix('.o')
-        self.dep_file = self.env.build_dir / 'objects' / source_file.with_suffix('.d')
+        self._modules_required = []
+        self._modules_generated = []
         self.add_input_files(source_file)
-        self.add_output_files(self.dep_file)
+        self.add_output_files(self.object_file)
         return self
 
     def input_metadata(self):
@@ -73,10 +80,19 @@ class ScanDeps(core.Task):
             'include_path': self.env.include_path,
         }
 
+    async def pre_run(self):
+        # Do an early up-to-date check.
+        if self._uptodate():
+            cache = self.ctx.cache[self.id.mangled]
+
+            # If the early check indicates we're up to date, ensure all required modules are (re-)built before we proceed to the actual check.
+            for module in cache['result']['modules_required']:
+                await self.env.module_mapper.registry.module_required(module)
+
     async def run(self):
         source_file = self.source_file
         object_file = self.object_file
-        dep_file = self.dep_file
+        dep_file = object_file.with_suffix('.d')
         cmi_dir = self.env.build_dir / 'cmi'
 
         # Ensure output directory exists.
@@ -92,7 +108,7 @@ class ScanDeps(core.Task):
             if self.env.module_mapper is not None:
                 flags.extend([
                     '-fmodules-ts',
-                    self.env.module_mapper.gcc_arg,
+                    self.env.module_mapper.gcc_arg(source_file),
                 ])
 
         for define in self.env.defines:
@@ -103,11 +119,12 @@ class ScanDeps(core.Task):
         await subprocess([
             compiler,
             *flags,
-            '-MMD', '-E',
+            '-c',
             source_file,
-            '-MT', object_file,
+            '-o', object_file,
+            '-MMD',
             '-MF', dep_file,
-        ], stdout = asyncio.subprocess.DEVNULL)
+        ])
 
         depmap = {}
 
@@ -126,125 +143,30 @@ class ScanDeps(core.Task):
                     depmap[t].extend(deps)
 
         file_deps = []
-        module_deps = []
-        module_gens = []
 
         for f in depmap[str(object_file)]:
             if f.endswith('.c++m'):
-                f = self.env.build_dir / 'cmi' / f.replace('.c++m', '.gcm')
+                continue
 
             file_deps.append(pathlib.Path(f))
 
-        for dep in depmap[str(object_file)]:
-            if dep.endswith('.c++m'):
-                module_deps.append(dep[:-5])
-
-        for dep in depmap.get('.PHONY', []):
-            if dep.endswith('.c++m'):
-                module_gens.append(dep[:-5])
-
         # Add included files as input files to trigger a rerun of this task if any changes in the future.
-        self.add_input_files(*(f for f in file_deps if f.suffix != '.gcm'))
+        self.add_input_files(*(f for f in file_deps if f != source_file))
+
+        # Add generated CMI files as output files.
+        self.add_output_files(*(self.env.module_mapper.gcm_path(module) for module in self._modules_generated))
 
         return {
-            'file_deps': file_deps,
-            'module_deps': module_deps,
-            'module_gens': module_gens,
+            'modules_required': self._modules_required,
+            'modules_generated': self._modules_generated,
         }
-
-class Compile(core.Task):
-    env: Env
-
-    def __new__(cls, env, source_file):
-        try:
-            self = super().__new__(cls, env.ctx, ('compile', env.build_dir, source_file))
-        except core.TaskExists as e:
-            if e.task.env == env:
-                return e.task
-            raise
-
-        self.env = env
-        self.source_file = source_file
-        self.object_file = self.env.build_dir / 'objects' / source_file.with_suffix('.o')
-        self.scan_deps = ScanDeps(env, source_file)
-        self.dependencies.append(self.scan_deps)
-        #self.add_input_files(source_file) # This is added implicitly through scan_deps.
-        self.add_output_files(self.object_file)
-        return self
-
-    def input_metadata(self):
-        return super().input_metadata() | {
-            'toolchain_prefix': self.env.toolchain_prefix,
-            'toolchain_suffix': self.env.toolchain_suffix,
-            'flags': self.env.cflags if self.source_file.suffix == '.c' else self.env.cxxflags,
-            'defines': self.env.defines,
-            'include_path': self.env.include_path,
-        }
-
-    def dynamic_deps(self):
-        self.add_input_files(*self.scan_deps.result['file_deps'])
-
-        return []
-
-    async def run(self):
-        source_file = self.source_file
-        object_file = self.object_file
-        cmi_dir = self.env.build_dir / 'cmi'
-
-        # Ensure output directory exists.
-        object_file.parent.mkdir(parents = True, exist_ok = True)
-
-        if source_file.suffix == '.c':
-            compiler = f'{self.env.toolchain_prefix}gcc{self.env.toolchain_suffix}'
-            flags = self.env.cflags.copy()
-
-        else:
-            compiler = f'{self.env.toolchain_prefix}g++{self.env.toolchain_suffix}'
-            flags = self.env.cxxflags.copy()
-            if self.env.module_mapper is not None:
-                flags.extend([
-                    '-fmodules-ts',
-                    self.env.module_mapper.gcc_arg,
-                ])
-
-        for define in self.env.defines:
-            flags.extend(['-D', define])
-        for path in self.env.include_path:
-            flags.extend(['-I', path])
-
-        await subprocess([
-            compiler,
-            *flags,
-            '-c',
-            source_file,
-            '-o', object_file,
-        ])
 
     async def post_run(self):
         # Report that modules are built.
-        for m in self.scan_deps.result['module_gens']:
-            self.add_output_files(self.env.build_dir / f'cmi/{m}.gcm')
-
-class ModuleCheck(core.Task):
-    env: Env
-
-    def __new__(cls, env, target, source_files):
-        self = super().__new__(cls, env.ctx, ('module_check', env.build_dir, target))
-        self.env = env
-        self.scan_deps_tasks = [ScanDeps(env, f) for f in source_files]
-        self.dependencies.extend(self.scan_deps_tasks)
-        return self
-
-    async def run(self):
-        generated_modules = set()
-
-        for t in self.scan_deps_tasks:
-            generated_modules.update(t.result['module_gens'])
-
-        for t in self.scan_deps_tasks:
-            for m in t.result['module_deps']:
-                if m not in generated_modules:
-                    raise Exception(f'Module {m} required by {t.source_file} does not exist.')
+        registry = self.env.module_mapper.registry
+        for m in self.result['modules_generated']:
+            if not registry.module_exists(m):
+                registry.module_provided(m)
 
 class Link(core.Task):
     env: Env
@@ -253,7 +175,6 @@ class Link(core.Task):
         self = super().__new__(cls, env.ctx, ('link', env.build_dir, target))
         self.env = env
         self.target = target
-        self.dependencies.append(ModuleCheck(env, target, source_files))
         self.source_files = source_files,
         self.object_tasks = [Compile(env, f) for f in source_files]
         self.ld_script = ld_script
--- erect/lib/gcc/module_mapper.py
+++ erect/lib/gcc/module_mapper.py
@@ -1,10 +1,30 @@
 import asyncio
 
+class ModuleRegistry:
+    def __init__(self):
+        self.modules = {}
+
+    def _module_future(self, name):
+        if not name in self.modules:
+            self.modules[name] = asyncio.Future()
+
+        return self.modules[name]
+
+    async def module_required(self, name):
+        await self._module_future(name)
+
+    def module_provided(self, name):
+        self._module_future(name).set_result(None)
+
+    def module_exists(self, name):
+        return name in self.modules and self.modules[name].done()
+
 class Handler:
     def __init__(self, mapper, reader, writer):
         self.mapper = mapper
         self.reader = reader
         self.writer = writer
+        self.task = None
 
     async def read(self):
         while line := await self.reader.readline():
@@ -16,21 +36,29 @@ class Handler:
         self.writer.write(line.encode('utf-8'))
         #print(f'> {line!r}', file=sys.stderr)
 
-    def handle(self, command):
+    async def handle(self, command):
         match command:
-            case ('HELLO', '1', *_):
+            case ('HELLO', '1', compiler, ident):
+                self.task = self.mapper.env._task_from_ident(ident)
                 return 'HELLO 1 erect-modmap'
 
             case ('MODULE-REPO',):
                 return f'PATHNAME {self.mapper.cmi_dir}'
 
             case ('MODULE-EXPORT', module, *_):
-                return f'PATHNAME {module}.gcm'
+                return f'PATHNAME {self.mapper.gcm_name(module)}'
 
             case ('MODULE-IMPORT', module, *_):
-                return f'PATHNAME {module}.gcm'
+                assert self.task is not None
+                self.task._modules_required.append(module)
+                async with self.task.mark_suspended():
+                    await self.mapper.registry.module_required(module)
+                return f'PATHNAME {self.mapper.gcm_name(module)}'
 
             case ('MODULE-COMPILED', module):
+                self.mapper.registry.module_provided(module)
+                if self.task:
+                    self.task._modules_generated.append(module)
                 return 'OK'
 
             case ('INCLUDE-TRANSLATE', path):
@@ -40,23 +68,27 @@ class Handler:
                 return 'ERROR'
 
     async def run(self):
-        command_queue = []
-        async for line in self.read():
-            command_queue.append(line)
-            if command_queue[-1][-1] == ';':
-                *command_queue[-1], _ = command_queue[-1]
-                continue
+        try:
+            command_queue = []
+            async for line in self.read():
+                command_queue.append(line)
+                if command_queue[-1][-1] == ';':
+                    *command_queue[-1], _ = command_queue[-1]
+                    continue
 
-            while len(command_queue) > 1:
-                self.write(self.handle(command_queue.pop(0)) + ' ;')
-            self.write(self.handle(command_queue.pop(0)))
-            await self.writer.drain()
-        self.writer.close()
+                while len(command_queue) > 1:
+                    self.write(await self.handle(command_queue.pop(0)) + ' ;')
+                self.write(await self.handle(command_queue.pop(0)))
+                await self.writer.drain()
+        finally:
+            self.writer.close()
 
 class ModuleMapper:
-    def __init__(self, cmi_dir):
-        self.port = None
+    def __init__(self, env, cmi_dir):
+        self.env = env
         self.cmi_dir = cmi_dir
+        self.registry = ModuleRegistry()
+        self.port = None
 
     async def _handle_client(self, reader, writer):
         m = Handler(self, reader, writer)
@@ -66,7 +98,13 @@ class ModuleMapper:
         server = await asyncio.start_server(self._handle_client, host = '::1', port = None)
         self.port = server.sockets[0].getsockname()[1]
 
-    @property
-    def gcc_arg(self):
+    def gcc_arg(self, ident):
         assert self.port is not None
-        return f'-fmodule-mapper=localhost:{self.port}'
+        return f'-fmodule-mapper=localhost:{self.port}?{ident}'
+
+    def gcm_name(self, module):
+        module = module.replace('/', ',')
+        return f'{module}.gcm'
+
+    def gcm_path(self, module):
+        return self.cmi_dir / self.gcm_name(module)
